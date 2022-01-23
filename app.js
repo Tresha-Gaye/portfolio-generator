d5b73dce9e5d9f70bce5fabd4f109a55a8372937
Twigster <treshagaye@gmail.com>
2022-01-22 20:14:44 -0500

    used promise chain to clean up functions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const inquirer = require('inquirer');
+const {writeFile, copyFile} = require('.utils/generate-site.js');
 
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
@@ -186,28 +186,25 @@ const mockData = {
     ]
   };
 const pageHTML = generatePage(mockData);
-// promptUser()
-// .then(promptProject)
-// .then(portfolioData => {
-    
-    // const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./dist/index.html', pageHTML, err => {
-        if (err) {
-            console.log(err);
-            return;
-        }   
-        console.log('Portfolio complete! Check out index.html in this directory to see it!');
-    
-        fs.copyFile('./src/style.css', './dist/style.css', err => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log('Style sheet copied successfully!');
-        });
-    });
-// });
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+,then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
+});
 
 
 
